8a524b04f2804b45a09b028bee45b099bf1ed7bd
nao20010128nao <nao20010128@gmail.com>
2020-01-03 06:07:06 +0000

    最初

---
+++ ssv.js
@@ -0,0 +1,8 @@
+const funcs=[x=>x,require("./sin"),Math.sin];
+
+const PRECISE=1000;
+const MAX=Math.PI*5;
+for(let i=0;i<=PRECISE;i++){
+  const value=i*MAX/PRECISE;
+  console.log(funcs.map(a=>a(value)).join(" "));
+}
